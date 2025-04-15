412bae55764b9da74ba8d082e971cb465b50841a
Regi Adi <regiadi.priyanto@gmail.com>
2025-04-15 22:05:43 +0700

    Add rust standalone function call from lua

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 use mlua::prelude::*;
 
+fn jump(_lua: &Lua, name: String) -> LuaResult<()> {
+    println!("Jump, {}!", name);
+    Ok(())
+}
+
 fn main() -> LuaResult<()> {
     // create a new lua state
     let lua = Lua::new();
@@ -7,17 +12,22 @@ fn main() -> LuaResult<()> {
     // load and execute a simple lua chunk
     lua.load("print('Hello from Lua!')").exec()?;
 
-    // create a rust function that can be called from lua    
+    // create a rust closure that can be called from lua    
     let hello_rust = lua.create_function(|_, name: String| {
         println!("Hello from Rust, {}!", name);
         Ok(())
     })?;
     
-    // set the rust function as a global in lua
+    // set the rust closure as a global in lua
     lua.globals().set("hello_rust", hello_rust)?;
 
-    // execute lua code that calls the rust function
+    // execute lua code that calls the rust closure
     lua.load("hello_rust('Lua User')").exec()?;
+
+    // create callable rust function from lua
+    let jump_fn = lua.create_function(jump)?;
+    lua.globals().set("jump", jump_fn)?;
+    lua.load("jump('Lua')").exec()?;
 
     Ok(())
 }
